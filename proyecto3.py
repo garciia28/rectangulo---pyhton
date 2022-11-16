8da4ae4149045250f051ac6917b3059daec91da4
Diego García <91524690+garciia28@users.noreply.github.com>
2022-11-16 14:12:39 +0100

    Add files via upload

---
+++ proyecto3.py
@@ -0,0 +1,7 @@
+base = float(input("Dime la base de tu rectángulo: "))
+altura = float(input("Dime la altura de tu rectángulo: "))
+
+perímetro = (2*base) + (2* altura) 
+area = base * altura
+
+print(f"El perímetro del rectángulo es {round(perímetro)} y su area es {round(area)}")
